615cfe06319cf123dfc00f271a7cf89f4492c6d3
rbasuvaraj <rbasuvaraj@tikamobile.com>
2023-09-14 21:51:42 +0530

    fax update zoom

--- src/Components/FaxView.js
+++ src/Components/FaxView.js
@@ -5,6 +5,7 @@ import { pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 import { Button, Paper, Typography, Grid } from '@mui/material';
+import PagingTabs from './PagingTabs';
 
 export default function FaxView({ onReset }) {
   pdfjs.GlobalWorkerOptions.workerSrc = new URL(
@@ -15,6 +16,7 @@ export default function FaxView({ onReset }) {
   const [numPages, setNumPages] = useState();
   const [pageNumber, setPageNumber] = useState(1);
   const [pdfResponse, setPdfResponse] = useState();
+  const [zoomLevel, setZoomLevel] = useState(1);
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
@@ -26,7 +28,7 @@ export default function FaxView({ onReset }) {
       url: '/fax/faxPdf',
       responseType: 'arraybuffer',
     })
-    .then((response) => setPdfResponse(response.data));
+      .then((response) => setPdfResponse(response.data));
   }, []);
 
   const handleNextPage = () => {
@@ -41,12 +43,24 @@ export default function FaxView({ onReset }) {
     }
   };
 
+  const handleZoomIn = () => {
+    if (zoomLevel < 2) {
+      setZoomLevel(zoomLevel + 0.1);
+    }
+  };
+
+  const handleZoomOut = () => {
+    if (zoomLevel > 0.5) {
+      setZoomLevel(zoomLevel - 0.1);
+    }
+  };
+
   useEffect(() => {
     const handleScroll = (event) => {
       if (event.deltaY > 0) {
-        handleNextPage(); // Scroll down to go to the next page
+        handleNextPage();
       } else if (event.deltaY < 0) {
-        handlePreviousPage(); // Scroll up to go to the previous page
+        handlePreviousPage();
       }
     };
 
@@ -58,33 +72,79 @@ export default function FaxView({ onReset }) {
   }, [pageNumber, numPages]);
 
   return (
-    <Paper elevation={3} style={{ padding: '17px', maxWidth: '800px', margin: 'auto' }}>
-      <Document file={pdfResponse} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <Typography variant="body1" align="center">
-        Page {pageNumber} of {numPages}
-      </Typography>
-      <Grid container justifyContent="center" spacing={2}>
-        <Grid item>
-          <Button
-            variant="contained"
-            onClick={handlePreviousPage}
-            disabled={pageNumber === 1}
-          >
-            Previous
-          </Button>
+    <div style={{ height: '100%', width: '100%', position: 'relative' }}>
+      <PagingTabs />
+      <div
+        style={{
+          position: 'absolute',
+          top: '10rem',
+          left: '20rem',
+          zIndex: 1,
+          
+          
+        }}
+      >
+        <Button
+          variant="contained"
+          onClick={handleZoomIn}
+          disabled={zoomLevel >= 2}
+          size='small'
+          style={{
+            margin: '1rem',
+
+        
+        
+        }}
+        >
+          +
+        </Button>
+        <br/>
+        <Button
+          variant="contained"
+          onClick={handleZoomOut}
+          size='small'
+          disabled={zoomLevel <= 0.5}
+        >
+          -
+        </Button>
+      </div>
+      <Paper
+        elevation={3}
+        style={{
+          padding: '20px',
+          maxWidth: '300px',
+          margin: 'auto',
+          transform: `scale(${zoomLevel})`, // Apply zoom level to page content
+          transformOrigin: 'top left', // Zoom from the top left corner
+        }}
+      >
+        <Document file={pdfResponse} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} width={300} />
+        </Document>
+        <Typography variant="body1" align="center">
+          Page {pageNumber} of {numPages}
+        </Typography>
+        <Grid container justifyContent="space-between" spacing={2}>
+          <Grid item>
+            <Button
+              variant="contained"
+              onClick={handlePreviousPage}
+              disabled={pageNumber === 1}
+            >
+              Previous
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              onClick={handleNextPage}
+              disabled={pageNumber === numPages}
+            >
+              Next
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Button
-            variant="contained"
-            onClick={handleNextPage}
-            disabled={pageNumber === numPages}
-          >
-            Next
-          </Button>
-        </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </div>
   );
 }
